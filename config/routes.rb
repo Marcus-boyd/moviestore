8bed359c2f79b4c30286576e26ce15b1db97bea7
Marcus-boyd <marcus.boyd389@gmail.com>
2018-02-04 00:01:21 -0500

    add divse

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path_name: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root 'movies#index'
   resources :movies, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
